4f1e32bcc4c88eaeeaa08031d858e399a5d97d46
Fernando <Fernando@192.168.1.6>
2014-04-02 12:11:12 -0300

    Delete old/

--- site/old/tool_pageNum.php
+++
@@ -1,5 +0,0 @@
-<select name="maxRows" onChange="listing_opts.submit();">
-<option <?php if($maxRows_retorno == 48) echo "selected"; ?>>48</option>
-<option <?php if($maxRows_retorno == 64) echo "selected"; ?>>64</option>
-<option <?php if($maxRows_retorno == 80) echo "selected"; ?>>80</option>
-</select>
